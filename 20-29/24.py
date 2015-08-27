d195bdbd5722b13435d519cb39ee97d0978edc28
Joao Pedro Alvito <jpfa@kth.se>
2015-08-27 12:01:18 +0000

    29 problems solved

--- 20-29/24.py
+++ 20-29/24.py
@@ -1,27 +1,2 @@
-import math
-def fac(n):
-	return math.factorial(n)
-
-a = [0,1,2,3,4,5,6,7,8,9]
-
-num = ''
-
-while 1:
-	for i in range(9,0,-1):
-		print "i: " + str(i)
-		for j in xrange(1,10):
-			print "j: " + str(j)
-			print "fac(i)*j: " + str(fac(i)*j)
-			if fac(i)*j > 1000000:
-				print "a[j-2]: " + str(a[j-2])
-				num = num + str(a[j-2])
-				a.remove(a[j-2])
-				print "a: " + str(a)
-				print "num: " + str(num)
-				break
-		print "num: " + str(num)
-		if a==[]:
-			break
-	if a == []:
-		break
-print num 
+import itertools
+print sorted([int(''.join(num)) for num in itertools.permutations('0123456789')])[999999]
