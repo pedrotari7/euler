24a8b62b341a312d04a49d66c1bb98c1587a1594
Joao Pedro Alvito <jpfa@kth.se>
2015-08-28 09:19:54 +0000

    35 problems solved

--- 30-39/34.py
+++ 30-39/34.py
@@ -1,13 +1,3 @@
 import math
-def fac(n):
-	return math.factorial(n)
-
-sum_fac = []
-for i in xrange(1,999999999):
-
-	print "i: " + str(i)
-
-	if i == sum([fac(int(ele)) for ele in str(i)]):
-		sum_fac.append(i)
-
-print "sum_fac: " + str(sum_fac)
+s = [n for n in xrange(10,100000) if n == sum([math.factorial(int(i)) for i in str(n)])]
+print sum(s)
