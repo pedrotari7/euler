ab4bfdca5aae17689ff197bf3e08f0c5b8511c92
Joao Pedro Alvito <jpfa@kth.se>
2015-08-28 12:40:18 +0000

    38 problems solved

--- 20-29/23.py
+++ 20-29/23.py
@@ -1,17 +1,17 @@
+import itertools
+
 def factors(n):
 	f = list(set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
 	f.remove(n)
 	return f
 
-def is_abundant(n):
-	return sum(factors(n)) > n
+abund = [n for n in range(1,28124) if sum(factors(n)) > n]
 
-def is_perfect(n):
-	return sum(factors(n)) == n
+nums = set([combi[0]+combi[1] for combi in itertools.combinations(abund+abund,2) if combi[0]+combi[1] < 28124])
 
-abund = []
-for n in range(1,28124):
-	if is_perfect(n):
-		abund.append(n)
+special = []
+for num in range(1,28124):
+    if num not in nums:
+        special.append(num)
 
-print abund
+print sum(special)
