b63218c9b8f78a5c19e3772cb3609e62aba52d82
Joao Pedro Alvito <jpfa@kth.se>
2015-08-31 11:40:06 +0000

    updated 42.py

--- 50-59/53.py
+++ 50-59/53.py
@@ -1,3 +1,2 @@
-import math
-
-print len( [(n,r) for n in xrange(23,101) for r in [r  for r in xrange(2,n+1) if math.factorial(n) > (10**6)*math.factorial(r)*math.factorial(n-r)]])
+from math import factorial
+print len( [(n,r) for n in xrange(23,101) for r in [r  for r in xrange(2,n+1) if factorial(n) > (10**6)*factorial(r)*factorial(n-r)]])
