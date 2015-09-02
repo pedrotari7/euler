be8873192d4f644197236effac506265ac9d9041
Joao Pedro Alvito <jpfa@kth.se>
2015-09-02 07:32:45 +0000

    50.py edited online with Bitbucket

--- 50-59/50.py
+++ 50-59/50.py
@@ -1,37 +1,34 @@
 import math
-primes = [2]
-i=2
 
-while i<1000001:
-
-	for num in primes:
-
-		if i%num == 0:
-			break
-
-		if num > math.sqrt(i):
-			primes.append(i)
-			break
-
-
-	i += 1
+def gen_primes(n):
+    primes = [2]
+    i=2
+    while i<=n:
+    	for num in primes:
+    		if i%num == 0:
+    			break
+    		if num > math.sqrt(i):
+    			primes.append(i)
+    			break
+    	i += 1
+    return primes
 
 
 max_sim = (0,0)
-
+primes = gen_primes(10**6)
+stop = False
 for i in xrange(len(primes)):
-	for j in xrange(i+2,len(primes)):
-		#print '({},{})'.format(i,j)
-		if sum(primes[i:j]) ==2:
-			continue
-		if sum(primes[i:j]) < primes[-1]:
-			if sum(primes[i:j]) in primes:
-				#print 'solution = ({},{})'.format(j-i,sum(primes[i:j]))
-				if (j-i) > max_sim[1]:
-					max_sim = (sum(primes[i:j]),j-i)
-					#print primes[i:j]
-					print max_sim
-		else:
-			break
+    for j in xrange(i+2,len(primes)):
+        if sum(primes[i:j]) ==2:
+            continue
+        if sum(primes[i:j]) < primes[-1]:
+            if sum(primes[i:j]) in primes:
+                if (j-i) > max_sim[1]:
+                    max_sim = (sum(primes[i:j]),j-i)
+        else:
+            stop = True
+            break
+    if stop:
+        break
 
 print max_sim
