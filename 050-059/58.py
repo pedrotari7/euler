372007186ddfaf1c64baff09a4fe5c8b642fc627
pedrotari7 <jpfa@kth.se>
2015-09-05 17:24:23 +0200

    updated diagonal algorithm on problem 58

--- 050-059/58.py
+++ 050-059/58.py
@@ -1,11 +1,9 @@
 import math
 import time
 
-def update_primes(primes,max_value,last):
+def update_primes(primes,max_value):
 
-	i=last
-
-	new_primes = []
+	i=primes[-1]
 
 	while i<max_value+1:
 
@@ -15,93 +13,58 @@ def update_primes(primes,max_value,last):
 				break
 
 			if num > math.sqrt(i):
-				new_primes.append(i)
-				last = i
+				primes.append(i)
 				break
 		i += 1
 
-	new_set = primes | set(new_primes)
-	return last, new_set
+	return primes
 
 
-def get_diag_numbers(grid):
-	diag = []
-
-	for i in xrange(len(grid)):
-		for j in xrange(len(grid)):
-			if j == i or j+i == len(grid)-1:
-				diag.append(grid[i][j])
-
-	return diag
-
-
-def create_grid(n,grid=[]):
-
-	if n == 1:
-		grid = [1]
-		return grid
- 	
-	start = n-3
-	if n > 3:
-		j = grid[start][start]
-	else:
-		grid  = [[5,4,3],[6,1,2],[7,8,9]]
-		return grid
-
-	while sum([len(line) for line in grid]) < n*n:
-
-		for ind in xrange(n-3,-1,-1):
-			j+=1
-			grid[ind].append(j)
-
-		grid = [range(j+n,j,-1)] + grid
-		j += n
-		
-		for ind in xrange(1,n-1):
-			j+=1
-			grid[ind] = [j] + grid[ind]
-
-
-		grid = grid + [range(j+1,j+1+n)]
-		j += n
-
-	return grid
-
-
-
-grid = [1]
-primes = set([2])
-i = 3
-last =2
+primes = [2]
+i = 1
+last =100
 per = 100
-while per>10:
-	ini1 = time.time()
-	grid = create_grid(i,grid)
-	stop1 = time.time()-ini1
+current_diag = []
+found = False
+while not found:
 
 
-	ini = time.time()
-	diag = get_diag_numbers(grid)
-	stop2 = time.time()-ini
+	tr = i**2
+	tl = i**2-i+2
+	bl = i**2-2*i+2
+	br = i**2-3*i+3
 
-	max_value = max(diag)
+	new = [tr,tl,bl,br] 
 
-	ini = time.time()
+
+	max_value = max(new)
+
 	if max_value > last:
-		last,primes = update_primes(primes,max_value,last)
-	stop3 = time.time()-ini
+		primes = update_primes(primes,max_value)
 
-	ini = time.time()
 
-	per = int(round(sum([num in primes for num in diag])/float(len(diag))*100))
-	stop4 = time.time()-ini
+	for new_value in new:
+		if new_value in primes:
+			current_diag.append(new_value)
+
+
+
+	per = (len(current_diag)/float(2*i-1))*100
+
+	if per < 10 and last> 10 and int(round(per)) == 10:
+		found = True
+		print i
+		break
+
+	last = per
 
 	print "n={} with {}% of primes".format(i,per) 
-	#print "grid:" + str(int(stop1*100))
-	#print "diag:" + str(int(stop2*1000))
-	#print "primes:" + str(int(stop3*1000))
-	#print "per1:" + str(int(stop4*1000))
-	#print "per2:" + str(int(stop5*1000))
-	#print "total:" + str(int((time.time()-ini1)*1000))
 
-	i+=2
+	if per > 10.5:
+		i+=10
+	elif per < 10:
+		i-=1
+	else:
+		i+=1
+
+
