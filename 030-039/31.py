1357e15a824e2a94e8828b55eeb14ffcda46232c
Joao Pedro Alvito <jpfa@kth.se>
2015-09-10 12:35:10 +0000

    corrected 31.py edited online with Bitbucket

--- 030-039/31.py
+++ 030-039/31.py
@@ -1,44 +1,29 @@
+a = [1,2,5,10,20,50,100]
+ways = 0
+m = 200
+for w0 in xrange(0,m/a[0]+1):
+    for w1 in xrange(0,m/a[1]+1):
+        if  w0*a[0] + w1*a[1] > m:
+            break
+        for w2 in xrange(0,m/a[2]+1):
+            if w0*a[0]+w1*a[1]+w2*a[2] > m:
+                break
+            for w3 in xrange(0,m/a[3]+1):
+                if w0*a[0]+w1*a[1]+w2*a[2]+w3*a[3] > m:
+                    break
+                for w4 in xrange(0,m/a[4]+1):
+                    if w0*a[0]+w1*a[1]+w2*a[2]+w3*a[3]+w4*a[4] > m:
+                        break
+                    for w5 in xrange(0,m/a[5]+1):
+                        if w0*a[0]+w1*a[1]+w2*a[2]+w3*a[3]+w4*a[4]+w5*a[5] > m:
+                            break
+                        for w6 in xrange(0,m/a[6]+1):
+                            total = w0*a[0]+w1*a[1]+w2*a[2]+w3*a[3]+w4*a[4]+w5*a[5]+w6*a[6]
+                            if total > m:
+                                break
+                            elif total == m:
+                                ways +=1
+                                print w0,w1,w2,w3,w4,w5,w6
 
-import itertools
-
-a = [0.01,0.05,0.1,0.2,0.5,1]
-
-lst = [list(ele) for ele in list(itertools.product([0, 1], repeat=len(a)))]
-
-combinations = []
-
-
-for comb in lst:
-
-	w = comb
-
-	for w1 in xrange(1,201):
-		if w[0] != 0:
-			w[0] = w1
-		for w2 in xrange(1,101):
-			if w[1] != 0:
-				w[1] = w2
-			for w3 in xrange(1,21):
-				if w[2] != 0:
-					w[2] = w3
-				for w4 in xrange(1,11):
-					if w[3] != 0:
-						w[3] = w4
-					for w5 in xrange(1,5):
-						if w[4] != 0:
-							w[4] = w5
-						for w6 in xrange(1,2):
-							if w[5] != 0:
-								w[5] = w6
-
-							if  sum([c*b for c,b in zip(a,w)]) == 2:
-								combinations.append(w)
-								print len(combinations)
-								break
-
-
-print combinations
-print len(combinations)
-
-
+print ways+1
 # add +1 beacause of 200p coin
