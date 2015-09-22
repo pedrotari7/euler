46c56d37ea4b6bcf71384e30e8f9bde49581c87c
pedrotari7 <jpfa@kth.se>
2015-09-22 07:49:12 +0200

    73 better version

--- 070-079/73.py
+++ 070-079/73.py
@@ -1,13 +1,32 @@
-def farey (n):
-	trigg = False
-	a = (0, 1)
-	if trigg: yield a
-	b = (1, n)
-	while b[0] < n:
-		k = (n + a[1]) // b[1]
-		a, b = b, (k * b[0] - a[0], k * b[1] - a[1])
-		if a == (1, 3): trigg = True
-		if a == (1, 2): return
-		if trigg: yield a
+def fareyNeighbor(n, a = 2, b = 5, c = 3, d = 7):
+    '''Given a / b and c / d, where a / b and c / d are two farey neighbors 
+    in a valid farey sequence, returns the neighbor of c / d in the nth farey 
+    sequence''' 
+    while b + d < n:
+        a = a + c
+        b = b + d
+    return a, b
 
-print len(list(farey(12000))) - 1
+def fareyGenerator(n, a=0, b=1, c=1, d = None):
+    '''Given a / b and c / d, where a / b and c / d are two farey neighbors
+    in an nth farey sequence, returns the next item in the farey sequence''' 
+    if d == None:
+        d = n
+    while c <= n:
+        k = int((n+b)/d)
+        a, b, c, d = c, d, k*c - a, k*d - b
+        yield Fraction(a, b)
+
+def fareyRange(n):
+    '''Return number of items between 1/3 and 1/2 in the nth farey sequence'''
+    (a, b) = fareyNeighbor(n, 2, 7, 1, 3) # calculate item to the left of 1/3
+    f_gen = fareyGenerator(n, a, b, 1, 3) # generate next item using (a, b)
+    p_q = f_gen.next()
+    count = 0
+    while p_q < Fraction(1, 2):
+        if p_q > Fraction(1, 3): count += 1
+        p_q = f_gen.next()
+    return count
+
+
+
