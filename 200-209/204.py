0dc4f21cc45a7671fd3d88e2e9f1b8d69ecf8b96
pedrotari7 <pedrotari7@gmail.com>
2017-12-11 08:53:39 +0100

    Problem 204

--- 200-209/204.py
+++ 200-209/204.py
@@ -1,25 +1,18 @@
 
+N = 10**9
+numbers = set()
+primes5 = [2, 3, 5]
+primes100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-numbers = []
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+def hamming(current,prev):
+	if current > N:
+		return False
+	numbers.add(current)
+	for next_p in [_ for _ in primes100 if _ >= prev]:
+		if not hamming(next_p*current,next_p): break
+	return True
 
+hamming(1,1)
 
-def hamming(current):
-
-	if current > 10**5:
-		return
-	else:
-		numbers.append(current)
-
-	for next_p in primes:
-		hamming(next_p*current)
-
-
-print len(primes)
-
-current = 1
-hamming(current)
-
-
-print len(numbers), len(set(numbers))
+print len(numbers)
 
