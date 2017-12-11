7c5a7de43d5994cd3dd70c56e5157dd250b74ad4
pedrotari7 <pedrotari7@gmail.com>
2017-12-11 19:29:54 +0100

    Problem 174

---
+++ 170-179/174.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+N = 10**6
+n = (N+4)//4
+
+sq = defaultdict(int)
+
+for i in xrange(n,2,-1):
+    prev = (i*4-4)
+    sq[prev]+=1
+    for j in xrange(i-2,2,-2):
+        nex = prev+(j*4-4)
+        if nex > N: break
+        prev = nex
+        sq[prev]+=1
+
+total = sum(sq[t] == n for t in sq for n in xrange(1,11))
+print total
